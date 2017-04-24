6bc9e6f4adccc9a72d412c2917880cc48be30b0f
Jesse Meachum <jmeachum17@gmail.com>
2017-04-24 19:51:14 -0400

    Inclass work

--- cis237inclass6/Controllers/CarsController.cs
+++ cis237inclass6/Controllers/CarsController.cs
@@ -16,9 +16,64 @@ namespace cis237inclass6.Controllers
         private CarsJMeachumEntities db = new CarsJMeachumEntities();
 
         // GET: Cars
-        public ActionResult Index()
+        public ActionResult Index() //******************
         {
-            return View(db.Cars.ToList());
+            //Setup a variable to hold the cars data
+            DbSet<Car> CarsToFilter = db.Cars;
+
+            // Setup some strings to hold the data that might be in the session.
+            // If there is nothing in the session we can still use these variables
+            // as a default value
+            string filterMake = "";
+            string filterMin = "";
+            string filterMax = "";
+
+            
+
+            // Define a min and max for the cylinders
+            int min = 0;
+            int max = 16;
+
+            // Check to see if there is a value in the session, and if there is, assign it
+            // to the variable that we setup to hold the value
+            if (!string.IsNullOrWhiteSpace((string)Session["make"]))
+            {
+                filterMake = (string)Session["make"];
+            }
+
+            if (!string.IsNullOrWhiteSpace((string)Session["min"]))
+            {
+                filterMin = (string)Session["min"];
+                min = Int32.Parse(filterMin);
+            }
+
+            if (!string.IsNullOrWhiteSpace((string)Session["max"]))
+            {
+                filterMax = (string)Session["max"];
+                max = Int32.Parse(filterMax);
+            }
+
+            // Do the filter on the CarsToFilter Dataset. Use the where that we used before 
+            // when doing the last inclass, only this time send in more lamda expressions to
+            // narrow it down further. Since we setup the default values for each of the filter
+            // parameters, min, max and filterMake, we can count on this always running with no errors.
+            IEnumerable<Car> filtered = CarsToFilter.Where(car => car.cylinders >= min 
+                && car.cylinders <= max &&
+                car.make.Contains(filterMake));
+
+            // Convert the dataset to a list now that the query work is done on it.
+            // The view is expecting a list, so we convert the database set to a list
+            IEnumerable<Car> finalFiltered = filtered.ToList();
+
+            ViewBag.filterMake = filterMake;
+            ViewBag.filterMin = filterMin;
+            ViewBag.filterMax = filterMax;
+
+            // Return the view with the filtered selection of cars
+            return View(finalFiltered);
+
+            // What was originally here
+            //return View(db.Cars.ToList());
         }
 
         // GET: Cars/Details/5
@@ -123,6 +178,34 @@ namespace cis237inclass6.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        // Mark the method as Post since it is reached from a form submit
+        // Make sure to validate the antiforgeryToken too since we include it in the form
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Filter()
+        {
+            // Get the form data that we sent out of the request object.
+            // the string that is used as a key to get the data matches the
+            // name property of the form control
+            string make = Request.Form.Get("make");
+            string min = Request.Form.Get("min");
+            string max = Request.Form.Get("max");
+
+            // Now that we have the data pulled out from the request object,
+            // let's put it into the session so that other methods can have access to it
+            Session["make"] = make;
+            Session["min"] = min;
+            Session["max"] = max;
+
+            // Redirect to the index page
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult JSON()
+        {
+            return Json(db.Cars.ToList(), JsonRequestBehavior.AllowGet); // second parameter: If user wants GET request, its okay
         }
     }
 }
